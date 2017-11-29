02d1acdff6e7d5b9ceed619409e9162177b9ff25
Josh Davis <josh.davis@kellogg.northwestern.edu>
2017-11-28 23:22:37 -0600

    change

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-/*
+      /*
 You're in charge of managing the development of the new TMDB mobile application. The deadline
-is almost here, and your developer quits! You can tell the board that you can't deliver the 
+is almost here, and your developer quits! You can tell the board that you can't deliver the
 project, or you can finish it yourself. Luckily, you took an awesome introduction to software
 development course in b-school. You've got this.
 
@@ -8,7 +8,7 @@ To-do:
 
 - When a movie title is entered into the text field, figure out how to capture the value that's
   typed in the state object.
-- When the return key is pressed, use the value that's typed by the user to call the TMDB API 
+- When the return key is pressed, use the value that's typed by the user to call the TMDB API
   (already done), and store the first result in state. Pass this object (via props) to the Movie
   component (already written, but hard-coded), and display the movie's poster, backdrop, title,
   rating (average vote) and overview. In addition, clear out the existing search term so that
@@ -17,7 +17,7 @@ To-do:
 Hints:
 
 - Read the React documentation on the TextInput component –
-  https://facebook.github.io/react-native/releases/0.28/docs/textinput.html – pay particular 
+  https://facebook.github.io/react-native/releases/0.28/docs/textinput.html – pay particular
   attention to the onChangeText (event that occurs when typing into the TextInput) and also
   onSubmitEditing (event that occurs when the user presses the return key) - write event handler
   functions for both!
@@ -33,21 +33,15 @@ class Movie extends React.Component {
     return (
       <View style={styles.movie}>
         <Image style={styles.backdrop}
-               source={{uri: "http://image.tmdb.org/t/p/w500/sy3e2e4JwdAtd2oZGA2uUilZe8j.jpg"}} />
+               source={{uri: "http://image.tmdb.org/t/p/w500" + this.props.movie.backdrop_path}} />
         <View style={styles.posterContainer}>
-          <Image style={styles.poster} 
-                 source={{uri: "http://image.tmdb.org/t/p/w500/5aGhaIHYuQbqlHWvWYqMCnj40y2.jpg"}} />
+          <Image style={styles.poster} source={{uri: "http://image.tmdb.org/t/p/w500" + this.props.movie.poster_path}} />
         </View>
         <View style={styles.titleAndVotes}>
-          <Text style={styles.title}>The Martian</Text>
-          <Text style={styles.votes}>7.6</Text>
+          <Text style={styles.title}>{this.props.movie.title}</Text>
+          <Text style={styles.votes}>{this.props.movie.vote_average.toFixed(0)}</Text>
         </View>
-        <Text style={styles.overview}>
-          During a manned mission to Mars, Astronaut Mark Watney is presumed dead after a fierce
-          storm and left behind by his crew. But Watney has survived and finds himself stranded 
-          and alone on the hostile planet. With only meager supplies, he must draw upon his ingenuity, 
-          wit and spirit to subsist and find a way to signal to Earth that he is alive.
-        </Text>
+        <Text style={styles.overview}>{this.props.movie.overview}</Text>
       </View>
     );
   }
@@ -61,21 +55,43 @@ export default class App extends React.Component {
       movie: null
     }
   }
-  movieNameInputSubmitted() {
-    // Make the TMDB API call, receive results. Leave the next two lines alone.
-    let url = "http://api.themoviedb.org/3/search/movie?query=" + this.state.movieNameInput + "&api_key=8ad43d355fccbef40dc3527123bb25ff&language=en-US&page=1&include_adult=false";
-    fetch(url).then(response => response.json()).then(json => {
-      console.log(json);
-    });
+
+  movieNameInputChanged = (text) => {
+    this.setState( { movieNameInput: text} );
   }
+
+  movieNameInputSubmitted = () => {
+    console.debug("the input is: " + this.state.movieNameInput +" the movie is: " + this.state.movie )
+    // Make the TMDB API call and receive results
+    const apiKey = 'e9743662f5a39568d8e25225f2c97e09'
+
+    let url = "http://api.themoviedb.org/3/search/movie?query=" + this.state.movieNameInput
+    url    += "&api_key=" + apiKey
+    url    += "&language=en-US&page=1&include_adult=false"
+
+    fetch(url).then(response => response.json()).then(json => {
+      console.log(json)
+      this.setState({ movie: json.results[0]
+       });
+       console.debug("the movie is now: " + this.state.movie)
+      });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <TextInput style={styles.movieNameInput} 
+        <TextInput style={styles.movieNameInput}
                    placeholder="Enter a movie name!"
-                   placeholderTextColor="#aaa" />
-        {/*Conditionally show the Movie component, only if there's a movie in state (so not initially)*/}
-        {this.state.movie && <Movie />}
+                   placeholderTextColor="#aaa"
+                   onChangeText={this.movieNameInputChanged}
+                   onSubmitEditing={this.movieNameInputSubmitted}
+                   value={this.state.movie}
+                   autoFocus={true} />
+
+        {/* If there's a movie, use the Movie component to show it  */}
+
+        {this.state.movie && <Movie movie={this.state.movie} />}
+
       </View>
     );
   }
@@ -93,8 +109,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#222',
     color: '#fff',
-    width: '100%', 
-    height: '100%', 
+    width: '100%',
+    height: '100%',
     borderWidth: 0,
     fontSize: 26,
     fontWeight: '300',
